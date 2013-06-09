3480f70b32abb228ece7c84f51a8894a61f74a36
ballance <ballance@ballance-Latitude-E6430s>
2013-06-09 14:25:32 -0700

    Initial scripts

---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+extra_defs=""
+
+while test -n "$1"; do
+  case $1 in
+    -D*)
+      extra_defs="$extra_defs $1"
+      ;;
+    -*)
+      echo "[ERROR] Unknown option $1"
+      ;;
+    *)
+      echo "[ERROR] Unknown argument $1"
+      ;;
+  esac
+  shift
+done
+
+$ECLIPSE_HOME/eclipse \
+    -nosplash -application org.eclipse.ant.core.antRunner \
+    --launcher.suppressErrors \
+    -buildfile collect.xml      \
+    -verbose \
+    -Dos=linux -Dws=gtk -Darch=x86_64 $extra_defs collect
+

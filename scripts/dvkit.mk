35467bd0ebd396f6f9701729687d882906eaa764
Matthew Ballance <matt.ballance@gmail.com>
2018-01-03 13:54:06 -0800

    Add PSSTools, update SVEditor, update copyright on splash screen

--- scripts/dvkit.mk
+++ scripts/dvkit.mk
@@ -3,7 +3,7 @@ PRODUCT := dvkit
 PRODUCT_FILE := /net.sf.dvkit.feature/dvkit.product
 PRODUCT_SRCDIR := $(DVKIT_DIR)/dvkit
 # PRODUCT_FEATURE_DIR := $(PSSTOOLS_DIR)/psstools/features/net.sf.psstools.pssc.feature
-PRODUCT_VERSION := 1.7.0
+PRODUCT_VERSION := 1.8.0
 # PRODUCT_PRE_BUILD_TARGETS := $(PRODUCT)_pssc_mw2e
 PRODUCT_PKGS += \
 	cdt \
@@ -14,6 +14,7 @@ PRODUCT_PKGS += \
 	epic \
 	gef \
 	jdt \
+	psstools \
 	pydev \
 	scala_ide \
 	sveditor \
---
+++ scripts/packages/antlr4_ide.mk
@@ -0,0 +1,34 @@
+
+
+ifneq (1,$(RULES))
+
+ANTLR4_IDE_URL = https://raw.githubusercontent.com/antlr4ide/antlr4ide-eclipse-release/master
+
+SCALA_IDE_VERSION = 212
+SCALA_IDE_ZIP = scala_ide_update.zip
+SCALA_IDE_URL = http://download.scala-ide.org/sdk/lithium/e47/scala212/stable/update-site.zip
+SCALA_IDE_DIR = $(BUILD_TOOLS_DIR)/scala_ide
+SCALA_IDE_REPOS = $(call ECLIPSE_REPOSITORY_URL,$(SCALA_IDE_DIR)/base),$(call ECLIPSE_REPOSITORY_URL,$(ECLIPSE_PLATFORM_DIR))
+
+SCALA_IDE_FEATURES := org.scala-ide.sdt.feature.feature.group
+
+else
+
+$(BUILD_TOOLS_DIR)/scala_ide.unpack : $(PACKAGES_DIR)/$(SCALA_IDE_ZIP)
+	$(Q)if test ! -d `dirname $@`; then mkdir -p `dirname $@`; fi
+	$(Q)rm -rf $(SCALA_IDE_DIR)
+	$(Q)mkdir -p $(SCALA_IDE_DIR)
+	$(Q)cd $(SCALA_IDE_DIR) ; unzip $^
+	$(Q)touch $@
+	
+scala_ide.install : $(BUILD_TOOLS_DIR)/scala_ide.unpack \
+	gef.install launchbar.install tm_terminal_ctrl.install
+	$(Q)$(call ECLIPSE_INSTALL_IU, $(SCALA_IDE_FEATURES), $(SCALA_IDE_REPOS), \
+		SCALA_IDE, $(PARENT_DIR_A))
+	$(Q)touch $@
+	
+$(PACKAGES_DIR)/$(SCALA_IDE_ZIP) : 
+	$(Q)if test ! -d `dirname $@`; then mkdir -p `dirname $@`; fi
+	$(Q)wget -O $@ $(SCALA_IDE_URL)
+
+endif
